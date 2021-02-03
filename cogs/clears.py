c6898b7da6f91a8b96bbde753fcfc975865e2c4b
sphinx <sphinx.banaknight@gmail.com>
2021-02-03 16:55:38 +0800

    Update clears.py

--- cogs/clears.py
+++ cogs/clears.py
@@ -1662,7 +1662,7 @@ Siege Whites
                 print(f'send embed returned {e}')
             emojis = ['<:ballot_box_with_check:806442408287535124>', '<:regional_indicator_x:806442408287535124>']
             for emoji in emojis:
-                await ctx.add_reaction(emoji)
+                await embeded.add_reaction(emoji)
             
             await ctx.send(f'Total no. of Yes answers: {yuppie}')
             await ctx.send(f'Total no. of No answers: {noppie}')
