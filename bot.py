ee104634377bb230cbb414f5e597f2756a632864
sphinx <sphinx.banaknight@gmail.com>
2021-02-04 12:11:41 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,11 +97,11 @@ feedback_noangrypingplz = 'to avoid <:AngryPing:703193629489102888> from me, ple
 
 prefix = ["/"]
 description = "A bot for sheet+discord linking/automation."
-client = commands.Bot(command_prefix = prefix, description = description)
+botko = commands.Bot(command_prefix = prefix, description = description)
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-client.remove_command('help')
+botko.remove_command('help')
 
 def istimedeventformat(input):
     try:
