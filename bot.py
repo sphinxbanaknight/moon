050b66ab39b9533557ada943f4574fefad4c165d
sphinx <sphinx.banaknight@gmail.com>
2021-02-04 17:24:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -177,8 +177,12 @@ async def on_ready():
     for server in client.guilds:
         if server.id == sk_server:
             sphinx = server
+            break
+            
+    for server in client.guilds:
         if server.id == c_server:
             cresence = server
+            break
     #    elif server.id == bk_server:
     #        burger = server
 
